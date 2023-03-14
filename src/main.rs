f8183ca76209c3c66bd4667a652df73c554a629e
kjetil midtlie <kjetil.midtlie@gmail.com>
2023-03-14 23:34:04 +0100

    lo-fi mem struct, just &str aaall the way

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,53 @@
 use chrono::{DateTime, Utc};
 use clap::Parser;
+use std::path::{Path, PathBuf, MAIN_SEPARATOR};
 
 #[derive(Parser)]
 struct Cli {
-    path: std::path::PathBuf,
-    base_path: Option<std::path::PathBuf>,
+    path: PathBuf,
+    base_path: Option<PathBuf>,
+}
+
+const BAD_FILE_NAME: &str = "[Name-less]";
+
+#[derive(Debug)]
+struct ReOrganized<'a> {
+    stem: &'a str,
+    size: Option<u64>,
+    created: DateTime<Utc>,
+    parent_parts: Option<Vec<&'a str>>,
+    ext: Option<&'a str>,
+}
+impl<'a> ReOrganized<'a> {
+    fn new(p: &'a Path, base: Option<&PathBuf>) -> Result<Self, std::io::Error> {
+        // pass error
+        //
+        // TODO: make this better
+        let created: DateTime<Utc> = p.metadata()?.created().map(|d| d.into())?;
+
+        let stripped_p = base.and_then(|x| p.strip_prefix(x).ok());
+        let min_p = stripped_p.unwrap_or(p);
+        let stem = min_p
+            .file_stem()
+            .and_then(|f_st| f_st.to_str())
+            .unwrap_or(BAD_FILE_NAME);
+
+        // TODO: not sure if this unwrap is smart
+        // Is there a better fallback-val ??
+        let size = p.is_file().then(|| p.metadata().unwrap().len());
+
+        let parent_path = min_p.parent().and_then(|par| par.to_str());
+        let parent_parts = parent_path.map(|par_p| par_p.split(MAIN_SEPARATOR).collect());
+        let ext = p.extension().and_then(|ext| ext.to_str());
+
+        Ok(Self {
+            stem,
+            size,
+            created,
+            parent_parts,
+            ext,
+        })
+    }
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -13,20 +56,9 @@ fn main() -> Result<(), std::io::Error> {
     let p = &args.path;
     let base = &args.base_path;
 
-    // why as_ref()
-    let stripped_p = base.as_ref().map(|x| p.strip_prefix(x).ok()).flatten();
+    let ro = ReOrganized::new(p, base.as_ref())?;
 
-    // cannot pass error up (so unwrap sadly)
-    let p_len: Option<u64> = p.is_file().then(|| p.metadata().unwrap().len());
-
-    // pass error to main
-    let p_created: DateTime<Utc> = p.metadata()?.created().map(|d| d.into())?;
-
-    let p_ext = p.extension();
-
-    println!("p: {:?}, b: {:?}, stripped_p: {:?}", p, base, stripped_p);
-    println!("cre: {}, len: {:?}", p_created, p_len);
-    println!("{:?}", p_ext);
+    println!("{:?}", ro);
 
     Ok(())
 }
