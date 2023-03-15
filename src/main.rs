89492b384ebfecef64d7aadda20955f83c5a7552
kjetil midtlie <kjetil.midtlie@gmail.com>
2023-03-15 11:56:41 +0100

    reorg has no throws anymore

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use chrono::{DateTime, Utc};
 use clap::Parser;
-use std::path::{Path, PathBuf, MAIN_SEPARATOR};
+use std::{
+    path::{Path, PathBuf, MAIN_SEPARATOR},
+    time::SystemTime,
+};
 
 #[derive(Parser)]
 struct Cli {
@@ -9,21 +12,22 @@ struct Cli {
 }
 
 const BAD_FILE_NAME: &str = "[Name-less]";
+// const FALLBACK
 
 #[derive(Debug)]
 struct ReOrganized<'a> {
     stem: &'a str,
     size: Option<u64>,
-    created: DateTime<Utc>,
+    created: Option<DateTime<Utc>>,
     parent_parts: Option<Vec<&'a str>>,
     ext: Option<&'a str>,
 }
 impl<'a> ReOrganized<'a> {
-    fn new(p: &'a Path, base: Option<&PathBuf>) -> Result<Self, std::io::Error> {
+    fn new(p: &'a Path, base: Option<&PathBuf>) -> Self {
         // pass error
         //
         // TODO: make this better
-        let created: DateTime<Utc> = p.metadata()?.created().map(|d| d.into())?;
+        let created: Option<SystemTime> = p.metadata().and_then(|m| m.created()).ok();
 
         let stripped_p = base.and_then(|x| p.strip_prefix(x).ok());
         let min_p = stripped_p.unwrap_or(p);
@@ -32,21 +36,22 @@ impl<'a> ReOrganized<'a> {
             .and_then(|f_st| f_st.to_str())
             .unwrap_or(BAD_FILE_NAME);
 
-        // TODO: not sure if this unwrap is smart
-        // Is there a better fallback-val ??
-        let size = p.is_file().then(|| p.metadata().unwrap().len());
+        let size = p
+            .is_file()
+            .then(|| p.metadata().ok().map(|meta| meta.len()))
+            .flatten();
 
         let parent_path = min_p.parent().and_then(|par| par.to_str());
         let parent_parts = parent_path.map(|par_p| par_p.split(MAIN_SEPARATOR).collect());
         let ext = p.extension().and_then(|ext| ext.to_str());
 
-        Ok(Self {
+        Self {
             stem,
             size,
-            created,
+            created: created.map(|st| st.into()),
             parent_parts,
             ext,
-        })
+        }
     }
 }
 
@@ -56,7 +61,7 @@ fn main() -> Result<(), std::io::Error> {
     let p = &args.path;
     let base = &args.base_path;
 
-    let ro = ReOrganized::new(p, base.as_ref())?;
+    let ro = ReOrganized::new(p, base.as_ref());
 
     println!("{:?}", ro);
 
